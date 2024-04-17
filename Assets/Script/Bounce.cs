81d66e0d74779ee747038a17cdf5a2c3e8f8c3b1
AntoniEspinal <97680717+AntoniEspinal@users.noreply.github.com>
2024-04-17 13:38:15 -0400

    ok

--- Assets/Script/Bounce.cs
+++ Assets/Script/Bounce.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Bounce : MonoBehaviour
 {
     Rigidbody rb;
+    public float force = 1.0f;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,7 +15,12 @@ public class Bounce : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        rb = GetComponent<Rigidbody>();
+    }
+
+    public void OnCollisionEnter(Collision collision)
+    {
+        rb.AddForce(Vector3.up * force);
     }
     
 }
